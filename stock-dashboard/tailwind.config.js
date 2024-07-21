1377801da30aff18e502198abd35fe32a2e76b56
Aamon <sahib2565@gmail.com>
2024-07-21 16:10:40 +0200

    Changed some error inside files, now everything should work properly

--- stock-dashboard/tailwind.config.js
+++ stock-dashboard/tailwind.config.js
@@ -1,22 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        white: "white";
-        none: "none";
+        white: "white",
+        none: "none",
       },
       borderWidth: {
         1: "1px",
       },
       fontFamily: {
-        quicksand: ["QuickSand", "sans-serif"],
-      }
+        quicksand: ["Quicksand", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
