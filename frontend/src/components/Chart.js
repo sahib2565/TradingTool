935213c3c980a4676c4ef788f06de4b4e44c7c9a
Sahib Singh <85299873+sahib2565@users.noreply.github.com>
2024-08-22 22:28:20 +0200

    Add files via upload
    Uploaded files of the fronted part-2

---
+++ frontend/src/components/Chart.js
@@ -0,0 +1,102 @@
+import React, { useContext, useState, useEffect,  } from "react";
+import { mockHistoricalData } from "../constants/mock";
+import { convertUnixTimestampToDate } from "../utils/helpers/date-helper";
+import {
+  Area,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+  AreaChart,
+  Tooltip,
+} from "recharts";
+import Card from "./Card";
+import { chartConfig } from "../constants/config";
+import ChartFilter from "./ChartFilter";
+import ThemeContext from "../context/ThemeContext";
+import { fetchHistoricalData } from "../api/stock-api";
+import StockContext from "../context/StockContext";
+
+const Chart = () => {
+  const [data, setData] = useState(mockHistoricalData);
+
+  const [filter, setFilter] = useState("1W");
+
+  const { darkMode } = useContext(ThemeContext);
+  const { stockSymbol } = useContext(StockContext);
+
+
+  useEffect(() => {
+    const updateChartData = async () => {
+      try{
+        const result = await fetchHistoricalData(stockSymbol, filter);
+        console.log(setData(formatData(result)));
+      }
+      catch (error){
+        setData([]);
+        console.log(error);
+      };
+    }
+    updateChartData();
+  }, [stockSymbol, filter])
+
+  const formatData = (data) => {
+    return data.c.map((item, index) => {
+      return {
+        value: item.toFixed(2),
+        date: convertUnixTimestampToDate(data.t[index]),
+      };
+    });
+  };
+
+  return (
+    <Card>
+      <ul className="flex absolute top-2 right-2 z-40">
+        {Object.keys(chartConfig).map((item) => (
+          <li key={item}>
+            <ChartFilter
+              text={item}
+              active={filter === item}
+              onClick={() => {
+                setFilter(item);
+              }}
+            />
+          </li>
+        ))}
+      </ul>
+      <ResponsiveContainer>
+        <AreaChart data={data}>
+          <defs>
+            <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor={darkMode ? "#312e81" : "rgb(199 210 254)"}
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor={darkMode ? "#312e81" : "rgb(199 210 254)"}
+                stopOpacity={0}
+              />
+            </linearGradient>
+          </defs>
+          <Area // activeDot={{ onClick: (event, payload) => console.log(payload.payload.value) }}
+            type="monotone"
+            dataKey="value"
+            stroke="#312e81"
+            fillOpacity={1}
+            fill="url(#chartColor)"
+            strokeWidth={0.5}
+          />
+          <Tooltip
+            contentStyle={darkMode ? { backgroundColor: "#111827" } : null}
+            itemStyle={darkMode ? { color: "#818cf8" } : null}
+          />
+          <XAxis dataKey="date" />
+          <YAxis domain={["dataMin", "dataMax"]} />
+        </AreaChart>
+      </ResponsiveContainer>
+    </Card>
+  );
+};
+
+export default Chart;
