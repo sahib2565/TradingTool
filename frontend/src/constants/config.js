6ce1cb4d54ef04883f0fd78d0cc37746d2c28ae5
Aamon <sahib2565@gmail.com>
2024-08-22 22:42:02 +0200

    j

--- frontend/src/constants/config.js
+++
@@ -1,7 +0,0 @@
-export const chartConfig = {
-  "5D": { resolution: "1", days: 5, weeks: 0, months: 0, years: 0 },
-  "1MO": { resolution: "60", days: 0, weeks: 0, months: 1, years: 0 },
-  "3MO": { resolution: "D", days: 0, weeks: 0, months: 3, years: 0 },
-  "5Y": { resolution: "D", days: 0, weeks: 0, months: 0, years: 5 },
-
-};
--- frontend/src/context/StockContext.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-
-const StockContext = React.createContext();
-
-export default StockContext
